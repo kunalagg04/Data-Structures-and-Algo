c9619b6fef9678042de8cc5d3eed6bdf2c2daf51
kunalagg04 <kunalagg04@gmail.com>
2019-10-15 14:32:17 +0530

    LL

---
+++ 7.LinkedList/1_Creating.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+    int integer;
+    node* next;
+
+    //CONSTRUCTOR
+    node (int d){
+        integer = d;
+        next = NULL;
+    }
+};
+
+int main(){
+    return 0;
+}
